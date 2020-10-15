c8724fe5337fc512b702e69932123cd2c33bca3c
kazukiyokoo <kazuki102777@gmail.com>
2020-10-15 21:29:58 +0900

    create_Development_environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,14 @@ namespace :deploy do
       upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
+
+  set :default_env, {
+    rbenv_root: "/usr/local/rbenv",
+    path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+    AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+    AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  }
+  
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
