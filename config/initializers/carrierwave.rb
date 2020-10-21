1bad2095ec3cc3542fa1dc13436cc2f64569a459
kazukiyokoo <kazuki102777@gmail.com>
2020-10-21 14:00:42 +0900

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,36 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = 'readcom'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/readcom'
-  end
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'depbucket0819'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/depbucket0819'
 end
-
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#     region: 'ap-northeast-1'
-#   }
-
-#   config.fog_directory  = 'readcom'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/readcom'
-# end
