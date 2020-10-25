7f0d7e34d719799787db885bbd9494d83d4f7686
kazukiyokoo <kazuki102777@gmail.com>
2020-10-25 12:39:50 +0900

    ハンバーガーメニューの作成完了

--- db/migrate/20200911015553_create_books.rb
+++ db/migrate/20200911015553_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :book_name, null: false
+      t.string :title, null: false
       t.integer :genre_id, null: false
       t.text :output_tweets
       t.text :derection
