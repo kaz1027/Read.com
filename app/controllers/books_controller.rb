eaeca4285ec23e0d462834095f643bd97605d680
kazukiyokoo <kazuki102777@gmail.com>
2020-10-30 10:20:52 +0900

    一旦保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,19 +4,34 @@ class BooksController < ApplicationController
   end
 
   def new
-    @books = Book.new
+    @book = Book.new
+  end
+
+  def next
+    @book = Book.new(book_params)
+    render :new if @book.invalid?
   end
 
   def create
-    @books = Book.create(book_params)
+    @book = Book.new(create_book_params)
+    if @book.save
+      render 'index'
+    else
+      render 'new'
+    end
   end
 
   def show
-    @books = Book.includes(:user).order("created_at DESC")
+    @book = Book.includes(:user).order("created_at DESC")
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :image, :genre_id, :derection).merge(user_id: current_user.id)
+    params.require(:book).permit(:title, :genre_id).merge(user_id: current_user.id)
   end
+
+  def create_book_params
+    params.require(:book).permit(:title, :genre_id, :output_tweets, :derection).merge(user_id: current_user.id)
+  end
+
 end
