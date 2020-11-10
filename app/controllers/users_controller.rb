71ccf3ef3c9d43a71a721af72b6d413aae077ef9
kazukiyokoo <kazuki102777@gmail.com>
2020-11-10 17:02:01 +0900

    商品編集ページの実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = current_user.books
+    @books = current_user.books.order("created_at DESC")
   end
 end
